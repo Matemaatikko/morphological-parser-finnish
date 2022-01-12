fae8c86a64ae11b0074b274113a5f6e003d27ef9
Matemaatikko <juho_salmensuu@hotmail.com>
2022-01-12 10:13:37 +0200

    Change location of files

--- src/main/scala/morph_fin/utils/FilesLocation.scala
+++ src/main/scala/morph_fin/utils/FilesLocation.scala
@@ -5,7 +5,7 @@ import java.io.File
 object FilesLocation {
 
   val project_path = new File(FilesLocation.getClass.getProtectionDomain().getCodeSource().getLocation().getPath()).getParentFile.getParentFile.getParentFile.getAbsolutePath
-  val files_path = new File(project_path + "/src/main/files").getAbsolutePath
-  val result_path = new File(project_path + "/src/main/files/result").getAbsolutePath
-  val rules_path = new File(project_path + "/src/main/files/rules").getAbsolutePath
+  val files_path = new File(project_path + "/files").getAbsolutePath
+  val result_path = new File(project_path + "/files/result").getAbsolutePath
+  val rules_path = new File(project_path + "/files/rules").getAbsolutePath
 }
